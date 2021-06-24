4cd5ee6f995ff3a149ea9029170eddc4c4b31abd
ariniekanoviyanti <ariniekanoviyanti@gmail.com>
2021-06-24 08:28:53 +0700

    Json Movie DB

---
+++ JsonMovieDB/app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,12 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=ca4c76e5406f1a81db6f9511cadbb33b";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
